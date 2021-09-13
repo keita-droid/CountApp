b87345793c671960ed57af6ad6fa9037822783f3
keita-droid <b.boykeita.ki@gmail.com>
2021-09-13 01:13:00 +0000

    隠し機能の微修正

--- app/assets/javascripts/secret.js
+++ app/assets/javascripts/secret.js
@@ -2,10 +2,7 @@ function secret(){
   var spock = document.getElementById("spock");
   
   spock.addEventListener('click', function(){
-    var wrapper = document.getElementById('wrapper')
-    var div = document.createElement('div');
-    div.className = 'secret';
-    wrapper.appendChild(div);
+    var div = document.getElementById('secret')
     div.classList.add('reveal');
     div.addEventListener('webkitAnimationEnd', function(){
       div.classList.remove('reveal');
